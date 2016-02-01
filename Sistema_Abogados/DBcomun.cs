76322c57ac2b96ec3202c43261021f5b922224d9
albertraza <arbert.jr@gmail.com>
2016-02-01 15:04:08 -0300

    Create Connection Object

---
+++ Sistema_Abogados/DBcomun.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Sistema_Abogados.Properties;
+
+namespace Sistema_Abogados
+{
+    public class DBcomun
+    {
+        private static string getConnectionString()
+        {
+            return Sistema_Abogados.Properties.Settings.Default.AbogadosDBConnectionString;
+        }
+        public static SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection(getConnectionString());
+            con.Open();
+            return con;
+        }
+    }
+}
