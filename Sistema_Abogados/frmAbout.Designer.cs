6cc6a7c3e210370783232654096bfdee1ad16172
albertraza <arbert.jr@gmail.com>
2016-02-09 10:00:48 -0300

    Add new form About

---
+++ Sistema_Abogados/frmAbout.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Sistema_Abogados
+{
+    partial class frmAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblmadefor = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(45, 32);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(214, 24);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Sistema de Abogados";
+            // 
+            // lblmadefor
+            // 
+            this.lblmadefor.AutoSize = true;
+            this.lblmadefor.Location = new System.Drawing.Point(75, 141);
+            this.lblmadefor.Name = "lblmadefor";
+            this.lblmadefor.Size = new System.Drawing.Size(154, 13);
+            this.lblmadefor.TabIndex = 1;
+            this.lblmadefor.Text = "Creado por: Ing. Albert Ramirez";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(122, 76);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(60, 13);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "Version 1.1";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(85, 186);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(135, 13);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "E-Mail: arbert.jr@gmail.com";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(115, 269);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 314);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblmadefor);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmAbout";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Acerca de";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblmadefor;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
