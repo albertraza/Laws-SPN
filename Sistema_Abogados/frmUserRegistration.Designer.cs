b197cb5c2e84ef1ca4fc11701170628aecaaa613
albertraza <arbert.jr@gmail.com>
2016-02-10 11:08:01 -0300

    Add UserRegistration Desing

--- Sistema_Abogados/frmUserRegistration.Designer.cs
+++ Sistema_Abogados/frmUserRegistration.Designer.cs
@@ -28,11 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblPasswordVerification = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cbUserLevel = new System.Windows.Forms.ComboBox();
+            this.lblUserLevel = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblBackto = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(23, 26);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(198, 236);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(62, 268);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(111, 23);
+            this.btnSelectImage.TabIndex = 1;
+            this.btnSelectImage.Text = "Seleccionar Imagen";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(373, 56);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(320, 59);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(47, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Nombre:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(303, 108);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(64, 13);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Contraseña:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(373, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // lblPasswordVerification
+            // 
+            this.lblPasswordVerification.AutoSize = true;
+            this.lblPasswordVerification.Location = new System.Drawing.Point(266, 153);
+            this.lblPasswordVerification.Name = "lblPasswordVerification";
+            this.lblPasswordVerification.Size = new System.Drawing.Size(101, 13);
+            this.lblPasswordVerification.TabIndex = 7;
+            this.lblPasswordVerification.Text = "Repetir Contraseña:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(373, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // cbUserLevel
+            // 
+            this.cbUserLevel.FormattingEnabled = true;
+            this.cbUserLevel.Location = new System.Drawing.Point(373, 195);
+            this.cbUserLevel.Name = "cbUserLevel";
+            this.cbUserLevel.Size = new System.Drawing.Size(100, 21);
+            this.cbUserLevel.TabIndex = 8;
+            // 
+            // lblUserLevel
+            // 
+            this.lblUserLevel.AutoSize = true;
+            this.lblUserLevel.Location = new System.Drawing.Point(279, 198);
+            this.lblUserLevel.Name = "lblUserLevel";
+            this.lblUserLevel.Size = new System.Drawing.Size(88, 13);
+            this.lblUserLevel.TabIndex = 9;
+            this.lblUserLevel.Text = "Nivel de Usuario:";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(269, 268);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 10;
+            this.btnRegister.Text = "Registrar";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(478, 268);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(373, 268);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Limpiar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // lblBackto
+            // 
+            this.lblBackto.AutoSize = true;
+            this.lblBackto.Location = new System.Drawing.Point(508, 26);
+            this.lblBackto.Name = "lblBackto";
+            this.lblBackto.Size = new System.Drawing.Size(91, 13);
+            this.lblBackto.TabIndex = 13;
+            this.lblBackto.TabStop = true;
+            this.lblBackto.Text = "Regresar al Menu";
+            // 
+            // frmUserRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frmUserRegistration";
+            this.ClientSize = new System.Drawing.Size(644, 342);
+            this.Controls.Add(this.lblBackto);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.lblUserLevel);
+            this.Controls.Add(this.cbUserLevel);
+            this.Controls.Add(this.lblPasswordVerification);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSelectImage);
+            this.Controls.Add(this.pbImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmUserRegistration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro de Usuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblPasswordVerification;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cbUserLevel;
+        private System.Windows.Forms.Label lblUserLevel;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.LinkLabel lblBackto;
     }
 }
