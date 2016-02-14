accd81c56ba48c50d65aa53bdcbefb06d2f5b2e0
albertraza <arbert.jr@gmail.com>
2016-02-13 23:08:20 -0300

    Add search method and listing method

--- Sistema_Abogados/servicios.cs
+++ Sistema_Abogados/servicios.cs
@@ -34,12 +34,59 @@ namespace Sistema_Abogados
             int re = -1;
             using(SqlConnection con = DBcomun.getConnection())
             {
-                SqlCommand comand = new SqlCommand(string.Format("insert into services (cService, explanation, price, pre_bono) values ('{0}', '{1}', '{2}', '{3}')",
+                SqlCommand comand = new SqlCommand(string.Format("INSERT INTO services (cService, explanation, price, pre_bono) VALUES ('{0}', '{1}', '{2}', '{3}')",
                     pServicio.Servicio, pServicio.Descripcion, pServicio.Precio, pServicio.Abono), con);
                 re = comand.ExecuteNonQuery();
                 con.Close();
             }
             return re;
+        }
+        // method for listing all services.
+        public static List<servicios> listAllServices()
+        {
+            List<servicios> list = new List<servicios>();
+            using(SqlConnection con = DBcomun.getConnection())
+            {
+                SqlCommand comand = new SqlCommand("SELECT * FROM services", con);
+                SqlDataReader re = comand.ExecuteReader();
+                while (re.Read())
+                {
+                    servicios pServicios = new servicios();
+                    pServicios.ID = re["ID"].ToString();
+                    pServicios.Servicio = re["cService"].ToString();
+                    pServicios.Descripcion = re["explanation"].ToString();
+                    pServicios.Precio = re["price"].ToString();
+                    pServicios.Abono = re["pre_bono"].ToString();
+
+                    list.Add(pServicios);
+                }
+                con.Close();
+            }
+            return list;
+        }
+        // search engine for services
+        public static List<servicios> searchServices(string id, string service, string description, string price, string pre_bono)
+        {
+            List<servicios> list = new List<servicios>();
+            using(SqlConnection con = DBcomun.getConnection())
+            {
+                SqlCommand comand = new SqlCommand(string.Format("select * from services where ID like '{0}%' and cService like '{1}%' and explanation like '{2}%' and price like '{3}%' and pre_bono like '{4}%'",
+                    id, service, description, price, pre_bono), con);
+                SqlDataReader re = comand.ExecuteReader();
+                while (re.Read())
+                {
+                    servicios pServicios = new servicios();
+                    pServicios.ID = re["ID"].ToString();
+                    pServicios.Servicio = re["cService"].ToString();
+                    pServicios.Descripcion = re["explanation"].ToString();
+                    pServicios.Precio = re["price"].ToString();
+                    pServicios.Abono = re["pre_bono"].ToString();
+
+                    list.Add(pServicios);
+                }
+                con.Close();
+            }
+            return list;
         }
     }
 }
