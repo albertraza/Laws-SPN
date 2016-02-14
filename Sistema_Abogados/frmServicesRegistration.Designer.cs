a0915a9830116a6c3453a84e2d6f179e5c254d4b
albertraza <arbert.jr@gmail.com>
2016-02-13 22:52:59 -0300

    Fix registration services layout

--- Sistema_Abogados/frmServicesRegistration.Designer.cs
+++ Sistema_Abogados/frmServicesRegistration.Designer.cs
@@ -141,6 +141,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
             this.ClientSize = new System.Drawing.Size(624, 408);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnClear);
