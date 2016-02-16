8cf30e81d8d4fd4bc76ac52f32bf81ed40d62909
albertraza <arbert.jr@gmail.com>
2016-02-16 09:57:35 -0300

    Add documentation

--- Sistema_Abogados/DBcomun.cs
+++ Sistema_Abogados/DBcomun.cs
@@ -10,10 +10,12 @@ namespace Sistema_Abogados
 {
     public class DBcomun
     {
+        // method for getting the Connection string.
         private static string getConnectionString()
         {
             return Sistema_Abogados.Properties.Settings.Default.AbogadosDBConnectionString;
         }
+        // method for returning the Connection Open.
         public static SqlConnection getConnection()
         {
             SqlConnection con = new SqlConnection(getConnectionString());
