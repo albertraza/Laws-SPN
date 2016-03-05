da1d4b95b0972d1097358cc8ecb4b0f7bbd525ff
albertraza <arbert.jr@gmail.com>
2016-03-05 02:32:18 -0300

    Add alquiler search

---
+++ Sistema_Abogados/frmAlquilerSearch.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Sistema_Abogados
+{
+    partial class frmAlquilerSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAlquiler = new System.Windows.Forms.DataGridView();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNombreCli = new System.Windows.Forms.TextBox();
+            this.lblNombreCli = new System.Windows.Forms.Label();
+            this.txtApellidoCliente = new System.Windows.Forms.TextBox();
+            this.lblApellidoCli = new System.Windows.Forms.Label();
+            this.txtApellidoInqui = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombreInqui = new System.Windows.Forms.TextBox();
+            this.lblNombreInquilino = new System.Windows.Forms.Label();
+            this.txtApellidoGaran = new System.Windows.Forms.TextBox();
+            this.lblApellidoGarante = new System.Windows.Forms.Label();
+            this.txtNombreGarante = new System.Windows.Forms.TextBox();
+            this.lblNombreGarante = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.bntLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlquiler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAlquiler
+            // 
+            this.dgvAlquiler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlquiler.Location = new System.Drawing.Point(12, 151);
+            this.dgvAlquiler.Name = "dgvAlquiler";
+            this.dgvAlquiler.Size = new System.Drawing.Size(903, 314);
+            this.dgvAlquiler.TabIndex = 0;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(713, 33);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(21, 13);
+            this.lblID.TabIndex = 1;
+            this.lblID.Text = "ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(740, 30);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtNombreCli
+            // 
+            this.txtNombreCli.Location = new System.Drawing.Point(130, 30);
+            this.txtNombreCli.Name = "txtNombreCli";
+            this.txtNombreCli.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreCli.TabIndex = 4;
+            // 
+            // lblNombreCli
+            // 
+            this.lblNombreCli.AutoSize = true;
+            this.lblNombreCli.Location = new System.Drawing.Point(45, 33);
+            this.lblNombreCli.Name = "lblNombreCli";
+            this.lblNombreCli.Size = new System.Drawing.Size(82, 13);
+            this.lblNombreCli.TabIndex = 3;
+            this.lblNombreCli.Text = "Nombre Cliente:";
+            // 
+            // txtApellidoCliente
+            // 
+            this.txtApellidoCliente.Location = new System.Drawing.Point(130, 87);
+            this.txtApellidoCliente.Name = "txtApellidoCliente";
+            this.txtApellidoCliente.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidoCliente.TabIndex = 6;
+            // 
+            // lblApellidoCli
+            // 
+            this.lblApellidoCli.AutoSize = true;
+            this.lblApellidoCli.Location = new System.Drawing.Point(45, 90);
+            this.lblApellidoCli.Name = "lblApellidoCli";
+            this.lblApellidoCli.Size = new System.Drawing.Size(82, 13);
+            this.lblApellidoCli.TabIndex = 5;
+            this.lblApellidoCli.Text = "Apellido Cliente:";
+            // 
+            // txtApellidoInqui
+            // 
+            this.txtApellidoInqui.Location = new System.Drawing.Point(359, 87);
+            this.txtApellidoInqui.Name = "txtApellidoInqui";
+            this.txtApellidoInqui.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidoInqui.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Apellido Inquilino:";
+            // 
+            // txtNombreInqui
+            // 
+            this.txtNombreInqui.Location = new System.Drawing.Point(359, 30);
+            this.txtNombreInqui.Name = "txtNombreInqui";
+            this.txtNombreInqui.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreInqui.TabIndex = 8;
+            // 
+            // lblNombreInquilino
+            // 
+            this.lblNombreInquilino.AutoSize = true;
+            this.lblNombreInquilino.Location = new System.Drawing.Point(264, 33);
+            this.lblNombreInquilino.Name = "lblNombreInquilino";
+            this.lblNombreInquilino.Size = new System.Drawing.Size(89, 13);
+            this.lblNombreInquilino.TabIndex = 7;
+            this.lblNombreInquilino.Text = "Nombre Inquilino:";
+            // 
+            // txtApellidoGaran
+            // 
+            this.txtApellidoGaran.Location = new System.Drawing.Point(580, 87);
+            this.txtApellidoGaran.Name = "txtApellidoGaran";
+            this.txtApellidoGaran.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidoGaran.TabIndex = 14;
+            // 
+            // lblApellidoGarante
+            // 
+            this.lblApellidoGarante.AutoSize = true;
+            this.lblApellidoGarante.Location = new System.Drawing.Point(486, 90);
+            this.lblApellidoGarante.Name = "lblApellidoGarante";
+            this.lblApellidoGarante.Size = new System.Drawing.Size(88, 13);
+            this.lblApellidoGarante.TabIndex = 13;
+            this.lblApellidoGarante.Text = "Apellido Garante:";
+            // 
+            // txtNombreGarante
+            // 
+            this.txtNombreGarante.Location = new System.Drawing.Point(580, 30);
+            this.txtNombreGarante.Name = "txtNombreGarante";
+            this.txtNombreGarante.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreGarante.TabIndex = 12;
+            // 
+            // lblNombreGarante
+            // 
+            this.lblNombreGarante.AutoSize = true;
+            this.lblNombreGarante.Location = new System.Drawing.Point(486, 33);
+            this.lblNombreGarante.Name = "lblNombreGarante";
+            this.lblNombreGarante.Size = new System.Drawing.Size(88, 13);
+            this.lblNombreGarante.TabIndex = 11;
+            this.lblNombreGarante.Text = "Nombre Garante:";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(731, 122);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 15;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // bntLimpiar
+            // 
+            this.bntLimpiar.Location = new System.Drawing.Point(827, 122);
+            this.bntLimpiar.Name = "bntLimpiar";
+            this.bntLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.bntLimpiar.TabIndex = 16;
+            this.bntLimpiar.Text = "Limpiar";
+            this.bntLimpiar.UseVisualStyleBackColor = true;
+            this.bntLimpiar.Click += new System.EventHandler(this.bntLimpiar_Click);
+            // 
+            // frmAlquilerSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(927, 477);
+            this.Controls.Add(this.bntLimpiar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtApellidoGaran);
+            this.Controls.Add(this.lblApellidoGarante);
+            this.Controls.Add(this.txtNombreGarante);
+            this.Controls.Add(this.lblNombreGarante);
+            this.Controls.Add(this.txtApellidoInqui);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombreInqui);
+            this.Controls.Add(this.lblNombreInquilino);
+            this.Controls.Add(this.txtApellidoCliente);
+            this.Controls.Add(this.lblApellidoCli);
+            this.Controls.Add(this.txtNombreCli);
+            this.Controls.Add(this.lblNombreCli);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.dgvAlquiler);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmAlquilerSearch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Busqueda Alquiler";
+            this.Load += new System.EventHandler(this.frmAlquilerSearch_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlquiler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAlquiler;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNombreCli;
+        private System.Windows.Forms.Label lblNombreCli;
+        private System.Windows.Forms.TextBox txtApellidoCliente;
+        private System.Windows.Forms.Label lblApellidoCli;
+        private System.Windows.Forms.TextBox txtApellidoInqui;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombreInqui;
+        private System.Windows.Forms.Label lblNombreInquilino;
+        private System.Windows.Forms.TextBox txtApellidoGaran;
+        private System.Windows.Forms.Label lblApellidoGarante;
+        private System.Windows.Forms.TextBox txtNombreGarante;
+        private System.Windows.Forms.Label lblNombreGarante;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button bntLimpiar;
+    }
+}
---
+++ Sistema_Abogados/frmAlquilerSearch.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Abogados
+{
+    public partial class frmAlquilerSearch : Form
+    {
+        // method for cleaing all inputs.
+        private void clear()
+        {
+            txtApellidoCliente.Clear();
+            txtApellidoGaran.Clear();
+            txtApellidoInqui.Clear();
+            txtID.Clear();
+            txtNombreCli.Clear();
+            txtNombreGarante.Clear();
+            txtNombreInqui.Clear();
+            dgvAlquiler.DataSource = alquiler.listAll();
+        }
+        public frmAlquilerSearch()
+        {
+            InitializeComponent();
+        }
+        // when form is loaded.
+        private void frmAlquilerSearch_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvAlquiler.DataSource = alquiler.listAll();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // when buscar button is clicked.
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string ID, Nc, ac, ni, ai, ng, ag;
+            if(txtApellidoCliente.Text == string.Empty)
+            {
+                ac = "";
+            }
+            else
+            {
+                ac = txtApellidoCliente.Text;
+            }
+            if(txtApellidoGaran.Text == string.Empty)
+            {
+                ag = "";
+            }
+            else
+            {
+                ag = txtApellidoGaran.Text;
+            }
+            if(txtApellidoInqui.Text == string.Empty)
+            {
+                ai = "";
+            }
+            else
+            {
+                ai = txtApellidoInqui.Text;
+            }
+            if(txtID.Text == string.Empty)
+            {
+                ID = "";
+            }
+            else
+            {
+                ID = txtID.Text;
+            }
+            if(txtNombreCli.Text == string.Empty)
+            {
+                Nc = "";
+            }
+            else
+            {
+                Nc = txtNombreCli.Text;
+            }
+            if(txtNombreGarante.Text == string.Empty)
+            {
+                ng = "";
+            }
+            else
+            {
+                ng = txtNombreGarante.Text;
+            }
+            if(txtNombreInqui.Text == string.Empty)
+            {
+                ni = "";
+            }
+            else
+            {
+                ni = txtNombreInqui.Text;
+            }
+            try
+            {
+                dgvAlquiler.DataSource = alquiler.search(ID, ni, ai, Nc, ac, ng, ag);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // when Limpiar button is clicked.
+        private void bntLimpiar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
