112a61682fa2edaaea44cdb24652c211dc4ef691
albertraza <arbert.jr@gmail.com>
2016-03-19 18:52:14 -0400

    fix bug

--- Sistema_Abogados/Program.cs
+++ Sistema_Abogados/Program.cs
@@ -16,7 +16,7 @@ namespace Sistema_Abogados
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmFacturacion());
+            Application.Run(new frmSplash());
         }
     }
 }
